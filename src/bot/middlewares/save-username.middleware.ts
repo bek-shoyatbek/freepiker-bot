989ea4bff95c47c159455fbbfba5e1ffd9e8c17d
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-09-26 15:04:22 +0500

    save username middleware created

---
+++ src/bot/middlewares/save-username.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction } from "grammy";
+import { MyContext } from "../types/context";
+import { User } from "../../models/user.model";
+
+export async function trackRequest(ctx: MyContext, next: NextFunction) {
+  const user = await User.findOne({ telegramId: ctx.from?.id.toString() });
+  const username = ctx.from?.username;
+
+  if (user && !user?.username && username) {
+    user.username = username;
+    await user.save();
+  }
+  await next();
+}
