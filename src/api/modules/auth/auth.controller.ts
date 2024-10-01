9d30ebb534b90e8bc16a7b738193cd5d4faa4252
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-01 13:21:23 +0500

    feat: login completed

--- src/api/modules/auth/auth.controller.ts
+++ src/api/modules/auth/auth.controller.ts
@@ -5,6 +5,8 @@ export class AuthController {
   static async login(req: Request, res: Response, next: NextFunction) {
     const { username, password } = req.body;
 
+    console.log("Username ", username, "\n Password ", password);
+
     if (!username || !password) {
       return res.status(400).json({
         message: "username and password are required!",
@@ -13,6 +15,7 @@ export class AuthController {
 
     const token = await AuthService.login({ username, password });
 
+    console.log("Token : ", token);
     if (!token) {
       return res.status(400).json({
         message: "Invalid credentials",
