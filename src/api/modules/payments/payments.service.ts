4bb125e9abec3a73388ceca24c2ac6610ab719d1
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-26 15:41:23 +0500

    faet: payments api completed

--- src/api/modules/payments/payments.service.ts
+++ src/api/modules/payments/payments.service.ts
@@ -1,22 +1,27 @@
 import { IPayment, Payment } from "../../../models/payment.model";
+import { UserPlan } from "../../../models/user-plan.model";
 
 export class PaymentService {
   static async getAll(): Promise<any[]> {
     try {
-      const payments = await Payment.find()
+      const payments = await UserPlan.find()
         .populate(["userId", "planId"])
         .sort({
           status: "asc",
+          startDate: "desc",
         });
-      return payments?.map((payment) => {
+
+      const filteredPayments = payments?.filter((e) => e?.userId);
+
+      return filteredPayments?.map((payment) => {
         return {
           userId: payment.userId?._id as unknown as string,
           username:
             (payment.userId as any)?.username || (payment.userId as any)?.name,
           plan: (payment.planId as any)?.title,
           status: payment.status,
-          startDate: (payment.planId as any)?.startDate,
-          endDate: (payment.planId as any)?.endDate,
+          startDate: payment.startDate,
+          endDate: payment.endDate,
         };
       });
     } catch (err) {
