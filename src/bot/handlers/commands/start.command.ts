b0dc9d964397088d523ecd63dd2f20c2b9fb3027
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-27 18:23:25 +0500

    feat: from property when user first started

--- src/bot/handlers/commands/start.command.ts
+++ src/bot/handlers/commands/start.command.ts
@@ -4,12 +4,15 @@ import { showLanguageMenu } from "./language.command";
 
 export async function handleStart(ctx: MyContext) {
   ctx.session.onStart = true;
+  const from = ctx?.match?.toString()?.split("from")[1];
+
   const user = await User.findOne({ telegramId: ctx.from!.id.toString() });
   if (!user) {
     const newUser = new User({
       telegramId: ctx.from!.id.toString(),
       name: ctx.from!.first_name,
       username: ctx.from?.username,
+      from: from, // From where this user is coming...
     });
     await newUser.save();
     ctx.session.isNewUser = true;
