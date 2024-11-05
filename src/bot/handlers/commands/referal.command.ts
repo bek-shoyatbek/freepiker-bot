e29dbe7bc8af23abe299c4689d98287c03721e4e
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-05 14:03:45 +0500

    feat: referal link text fixed

--- src/bot/handlers/commands/referal.command.ts
+++ src/bot/handlers/commands/referal.command.ts
@@ -2,7 +2,12 @@ import { generateReferalLink } from "../../generators/referal";
 import { MyContext } from "../../types/context";
 
 export const onReferalCommand = async (ctx: MyContext) => {
-    const referalLink = generateReferalLink(ctx.from!.id.toString());
-    await ctx.reply(`Referal link: ${referalLink}`);
-    return;
-}
+  const referalLink = generateReferalLink(ctx.from!.id.toString());
+  await ctx.reply(
+    `Ushbu havolangizni do'stlarga yuborish orqali har bir tarif obunasi uchun siz 20 ta bepul fayl qo'yga kiritasiz. 🤩
+
+Sizning taklif havolangiz tayyor🥳: <a href="${referalLink}">link</a>`,
+    { parse_mode: "HTML" },
+  );
+  return;
+};
